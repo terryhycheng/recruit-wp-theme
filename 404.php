56af6503174c2fe9f32944e4ccc636c1ea19ef7b
terryhycheng <terryhycheng@gmail.com>
2023-05-01 15:15:35 +0100

    Added page hierarchy

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+
+<div>
+  <h1>Error Page</h1>
+</div>
+
+<?php get_footer() ?>
---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+
+<div>
+  <h1>Hello World!</h1>
+</div>
+
+<?php get_footer() ?>
