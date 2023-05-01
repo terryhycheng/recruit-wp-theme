4a8b96939ebb787e7e9681c9daedd756d3e8d20d
terryhycheng <terryhycheng@gmail.com>
2023-05-01 14:59:21 +0100

    Installed tailwindcss and cli

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#132745",
+        secondary: "#32FFCE",
+      },
+    },
+  },
+  plugins: [],
+};
