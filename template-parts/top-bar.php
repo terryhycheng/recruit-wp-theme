0f514a4cdbbfc66b9df9e601e41e7f455f9efee2
terryhycheng <terryhycheng@gmail.com>
2023-05-01 21:55:58 +0100

    Added a link to `index.php` to header's logo

--- template-parts/top-bar.php
+++ template-parts/top-bar.php
@@ -1,6 +1,8 @@
 <div class="bg-main text-white p-8">
   <div class="max-w-[1500px] mx-auto flex justify-between">
-    <h1 class="font-manrope font-bold text-2xl">Recruit.</h1>
+    <a href="/index.php">
+      <h1 class="font-manrope font-bold text-2xl">Recruit.</h1>
+    </a>
     <div class="relative w-10">
       <img src="<?php echo get_template_directory_uri() . "/assets/img/menu.svg" ?>" alt="menu-logo" id="menu-icon"
         class="object-cover cursor-pointer">
