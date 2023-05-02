13c79dce89d8a98c8dac64a48765c16f122988a4
terryhycheng <terryhycheng@gmail.com>
2023-05-02 18:14:31 +0100

    Imported all components to `single-jobs`

--- single-jobs.php
+++ single-jobs.php
@@ -1,7 +1,9 @@
 <?php get_header() ?>
 
-<div>
-  <h1>Single Job Page</h1>
+<?php get_template_part('template-parts/job/job-hero') ?>
+<div class="flex flex-col lg:flex-row gap-20 max-w-[1500px] mx-auto px-4 my-20">
+  <?php get_template_part('template-parts/job/job-sidebar') ?>
+  <?php get_template_part('template-parts/job/job-body') ?>
 </div>
 
 <?php get_footer() ?>
