ac48b3ccac3df43d1290e0bf540741ec0c1733ce
terryhycheng <terryhycheng@gmail.com>
2023-05-02 18:13:53 +0100

    Installed `line-clamp` and `typography` to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
